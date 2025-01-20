a18ed77efd2efaa562aaa7a1adefe9057bd92f71
Jonathan Hessel <hesseljonathan@gmail.com>
2025-01-20 14:22:26 +0100

    Add winndows build support

--- include/libcu132/libcu132.h
+++ include/libcu132/libcu132.h
@@ -2,6 +2,12 @@
 #define LIBCU132_H
 #include <stdbool.h>
 
+#ifdef _WIN32
+    #define LIBCU132_API __declspec(dllexport)
+#else
+    #define LIBCU132_API
+#endif
+
 typedef enum {
     SUCCESS = 0,
     ERROR = 1
@@ -21,14 +27,14 @@ typedef struct {
 typedef void (*CU_STATUS_CALLBACK_T)(CU_STATUS status);
 typedef void (*CU_DATA_CALLBACK_T)(unsigned char id, unsigned int timestamp, unsigned char sensor);
 
-CU_RESULT cu_init(CU132 **device);
-CU_RESULT cu_connect(CU132 *device, char *serial_fd);
-void cu_destroy(CU132 *device);
+LIBCU132_API CU_RESULT cu_init(CU132 **device);
+LIBCU132_API CU_RESULT cu_connect(CU132 *device, char *serial_fd);
+LIBCU132_API void cu_destroy(CU132 *device);
 
-void cu_register_status_callback(CU132 *device, CU_STATUS_CALLBACK_T callback);
-void cu_register_data_callback(CU132 *device, CU_DATA_CALLBACK_T callback);
+LIBCU132_API void cu_register_status_callback(CU132 *device, CU_STATUS_CALLBACK_T callback);
+LIBCU132_API void cu_register_data_callback(CU132 *device, CU_DATA_CALLBACK_T callback);
 
-CU_RESULT cu_poll(CU132 *device);
-CU_RESULT cu_get_version(CU132 *device, int *version);
+LIBCU132_API CU_RESULT cu_poll(CU132 *device);
+LIBCU132_API CU_RESULT cu_get_version(CU132 *device, int *version);
 
 #endif
